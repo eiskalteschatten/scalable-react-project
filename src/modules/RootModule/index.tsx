b169950a3e593bb9a8a0d3b06d0b0ebbfd43d275
Alex Seifert <alex@alexseifert.com>
2022-12-17 19:21:13 +0100

    Update the comments

--- src/modules/RootModule/index.tsx
+++ src/modules/RootModule/index.tsx
@@ -10,7 +10,6 @@ const RootModule: React.FC = () => {
   // The RootModule is responsible for everything that happens at the root levels and all of its content.
   // It is responsible for the home page (located at "/") as well as the 404 page.
   // It also controls the routes for all of its components.
-  // In thie particular example, we only have one component, so we only need one route.
 
   return (
     <MainLayout>
