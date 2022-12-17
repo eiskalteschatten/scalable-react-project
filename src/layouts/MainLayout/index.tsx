8e42c2f06f656bfe6656bd05a834db46a77ab5f4
Alex Seifert <alex@alexseifert.com>
2022-12-17 19:19:20 +0100

    Add the MainLayout component

---
+++ src/layouts/MainLayout/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './MainLayout.module.scss';
+
+const MainLayout: React.FC = () => {
+  // The main layout for the application
+
+  return (
+    <div>
+      MainLayout
+    </div>
+  );
+}
+
+export default MainLayout;
