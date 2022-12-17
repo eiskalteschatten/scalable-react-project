1fe2849c52ac2887cb693be65a9e0c60758ede84
Alex Seifert <alex@alexseifert.com>
2022-12-17 19:54:23 +0100

    Add the nav links to the main nav

--- src/layouts/MainLayout/MainNav/index.tsx
+++ src/layouts/MainLayout/MainNav/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import clsx from 'clsx';
 
 import styles from './MainNav.module.scss';
 
 const MainNav: React.FC = () => {
+  const navClassName = ({ isActive }: { isActive: boolean }) => clsx({
+    [styles.activeLink]: isActive,
+  });
+
   return (
     <div className={styles.mainNav}>
-      nav here
+      <NavLink to='/' className={navClassName} end>
+        Home
+      </NavLink>
+      <NavLink to='/privacy-statement' className={navClassName} end>
+        Privacy Statement
+      </NavLink>
     </div>
   );
 }
